c82404c1a7dab1edbb5781c05c5f92ec3ae304ab
RANJITHKUMAR99 <51471474+RANJITHKUMAR99@users.noreply.github.com>
2019-06-24 11:42:27 +0530

    Update 3.py

--- 3.py
+++ 3.py
@@ -2,7 +2,7 @@ a=raw_input()
 v=['a','e','i','o','u']
 c=['b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','y','z']
 if(a in v):
-    print('Vowels')
+    print('Vowel')
 elif(a in c):
     print('Consonant')
 else:
