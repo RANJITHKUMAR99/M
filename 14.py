8aa1c1394a36cf4f99e1d8bd2d79b9ad8f4a6d33
RANJITHKUMAR99 <51471474+RANJITHKUMAR99@users.noreply.github.com>
2019-07-12 18:20:03 +0530

    Create 14.py

---
+++ 14.py
@@ -0,0 +1,15 @@
+a=int(input())
+b=0
+if a<=1:
+  print("invalid")
+else:
+  for i in range(2,a):
+    if(a%i==0):
+      b=0
+      break
+    else:
+      b=1
+  if b==0:
+    print("no")
+  else:
+    print("yes")
